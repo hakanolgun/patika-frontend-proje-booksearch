ef6d471440061adb8942b48140d68faae82f5a00
hakanolgun <kaptanhakanolgun@gmail.com>
2021-07-27 14:55:55 +0300

    solves some keywords doesnt have author or image info

--- src/App.js
+++ src/App.js
@@ -44,13 +44,23 @@ function App() {
           return (
             <div className="card" key={index}>
               <img
-                src={item.volumeInfo.imageLinks.thumbnail} 
+                src={
+                  item.volumeInfo.imageLinks
+                    ? item.volumeInfo.imageLinks.thumbnail
+                    : "http://www.clker.com/cliparts/2/9/2/9/12427966971416347889No_photo_available.svg.hi.png"
+                }
                 alt={item.volumeInfo.title}
               />
               <div className="info">
                 <p className="info-title">{item.volumeInfo.title}</p>
-                <p className="info-authors">{item.volumeInfo.authors.join(", ")}</p>
-                <a className="info-link" href={item.volumeInfo.previewLink}>Details</a>
+                <p className="info-authors">
+                  {item.volumeInfo.authors
+                    ? item.volumeInfo.authors.join(", ")
+                    : "No author info"}
+                </p>
+                <a className="info-link" href={item.volumeInfo.previewLink}>
+                  Details
+                </a>
               </div>
             </div>
           );
